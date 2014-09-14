5c977c53e947cf1eee76987d7849ffbe2ac6e252
Mandy Yeung <mandycodes@gmail.com>
2014-09-14 09:14:21 -0400

    Updated feature spec

--- spec/features/domain_pattern_spec.rb
+++ spec/features/domain_pattern_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe 'predicting emails', :type => :feature do
 
   it 'will report no matches if domain is not in database' do
-    visit '/domain_patterns/new'
+    visit '/'
     fill_in('name', :with => 'Barack Obama')
     fill_in('domain', :with => 'whitehouse.gov')
-    click_button('Generate Email Address')
+    click_button('Submit')
     expect(page).to have_content('No email match found!')
   end
 end
