67ae3c85dd17a1e6d14aea9c0dad5d5480d7dbd8
Mandy Yeung <mandycodes@gmail.com>
2014-09-26 14:41:24 -0400

    Refactored controller

--- app/controllers/domain_patterns_controller.rb
+++ app/controllers/domain_patterns_controller.rb
@@ -1,8 +1,4 @@
 class DomainPatternsController < ApplicationController
-  def new
-    @domain_pattern = DomainPattern.new
-  end
-
   def show
     @first_name, @last_name = params[:name].downcase.split
     @domain = params[:domain]
